1fd62bf076675237cfc4aaf319638df498007bdf
Samantha <shangsan@ucla.edu>
2019-08-02 16:48:20 -0700

    update projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,15 +1,16 @@
 // TODO Add a couple lines about each project
 const data = [
     {
-        title: "Daily Forty-Niner",
-        subtitle: "2015 BVP Hackathon",
-        link: "https://daily49er.com",
-        image: "/images/projects/daily49er.png",
-        date: "2019-8-02",
-        desc:
-            "Built and re-designed website for the official student-run newspaper " +
-            "at California State University, Long Beach."
-    }
-];
-
-export default data;
+      title: 'Daily Forty-Niner',
+      subtitle: '2019 Daily Forty-Niner Website',
+      link: 'https://daily49er.com',
+      image: '/images/projects/daily49er.png',
+      date: '2019-08-02',
+      desc: 'Built and re-designed website for the official student-run newspaper '
+        + 'at California State University, Long Beach. '
+        + 'The Daily Forty-Niner provides offers local insight and coverage of events, trends and important topics on campus '
+        + 'to the students, faculty, staff and community members of CSULB and Long Beach.'
+    },
+  ];
+  
+  export default data;
