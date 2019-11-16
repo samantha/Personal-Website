061c07030e33311c2a088bd4593a7fc5743861cb
samantha <shangsan@ucla.edu>
2019-11-16 01:46:24 -0800

    Add UKS production

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,13 +4,21 @@ const data = [
       title: 'Daily Forty-Niner',
       subtitle: '2019 Daily Forty-Niner Website',
       link: 'https://daily49er.com',
-      image: '/images/projects/daily49er.png',
+      image: '/images/projects/Daily49er.png',
       date: '2019-08-02',
       desc: 'Built and re-designed website for the official student-run newspaper '
         + 'at California State University, Long Beach. '
         + 'The Daily Forty-Niner provides offers local insight and coverage of events, trends and important topics on campus '
         + 'to the students, faculty, staff and community members of CSULB and Long Beach.'
     },
+    {
+      title: 'Thoun Chey and the Golden Fish',
+      subtitle: 'UCLA United Khmer Students Culture Night',
+      link: 'https://pacificties.org/united-khmer-students-18th-culture-night-thoun-chey-and-the-golden-fish/',
+      image: 'https://pacificties.org/wp-content/uploads/2015/03/IMG_8775-3184x1592.jpg',
+      date: '2015-03-08',
+      desc: 'Wrote an adaptation of two popular Khmer folktales: “Thoun Chey” and “Moronak Meada." Both folktales are a representation of Cambodia’s art and literature; they are important because much of Cambodia’s art and literature was lost to the world forever since the Khmer Rouge. These tales have been preserved through the storytelling of the older generation and are now shared with the UCLA community. '
+    },
   ];
-  
+
   export default data;
